a9108f47ab62bb5f3664ea8a69fc17ffd9ef54f2
Sienna Kopf <princess.kopf@gmail.com>
2020-05-13 09:17:10 -0600

    Add tests for room functionality for category, length, width, and area

--- test/room_test.rb
+++ test/room_test.rb
@@ -9,16 +9,21 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
     room1 = Room.new(:bedroom, 10, '13')
     room2 = Room.new(:living_room, 15, '12')
     assert_equal 130, room1.area
     assert_equal 180, room2.area
+  end
+
+  def test_it_can_get_length_and_width
+    room = Room.new(:bedroom, 10, '13')
+
+    assert_equal 10, room.length
+    assert_equal '13', room.width
   end
 end
