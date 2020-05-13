a03ad665f1bb88c090813d1df0d439b606df72e0
Sienna Kopf <princess.kopf@gmail.com>
2020-05-13 09:39:43 -0600

    Add test for house functionality above market average

--- test/house_test.rb
+++ test/house_test.rb
@@ -30,4 +30,11 @@ class HouseTest < Minitest::Test
     assert_equal [@room_1, @room_2], @house.rooms
   end
 
+  def test_it_is_above_market_average
+    assert_equal false, @house.above_market_average?
+
+    expensive_house = House.new("$600000", '987 strawberry fields ave')
+    assert_equal true, expensive_house.above_market_average?
+  end
+
 end
